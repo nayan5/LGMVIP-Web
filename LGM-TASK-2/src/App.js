a69409224b3e7b35d9d14d4d3b517a506503665a
nayan5 <65781187+nayan5@users.noreply.github.com>
2022-02-25 18:55:22 +0530

    TASK 2 ADDED

---
+++ LGM-TASK-2/src/App.js
@@ -0,0 +1,51 @@
+import Users from "./cards.js";
+import './App.css';
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={users_data :[],
+                loading : true
+              }
+
+              this.updateState = this.updateState.bind(this)
+  }
+
+  updateState(){
+    console.log("VT");
+    document.getElementById("main").style.display='flex';
+    const link="https://reqres.in/api/users?page=1";
+    fetch(link)
+    .then(response => response.json())
+    .then((users) => {
+      this.setState({users_data :users.data,
+                      loading: false
+                    })
+      console.log(users.data);
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+
+  render(){
+    return (<>
+      <nav className="navbar">
+        <div className="nav-items">
+          <h3 class="first-h2">LetsGrowMore TASK - 2</h3>
+          <button class="but" onClick={this.updateState}>Get Users</button>
+          </div>
+        </nav>    
+          
+      <Users loading={this.state.loading} users={this.state.users_data}/>
+      <footer className="footer"><b>CLICK THE BUTTON TO GET RESULT!</b></footer>
+      </>   
+          ) 
+  }
+}
+
+export default App;
